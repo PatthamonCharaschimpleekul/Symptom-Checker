a4b93c690bf98c6aeb57dae5e94a3291b9288363
Toroko <patthamon.chr@gmail.com>
2026-03-10 15:58:21 +0800

    edited typo

--- Symptom-Checker/script.js
+++ Symptom-Checker/script.js
@@ -1,48 +1,51 @@
 let dataset = {};
 
-//download dataset from Json file
+// download dataset from Json file
 fetch("data/symptom_data.json")
-    .then(response => response.json())
-    .then(data => {
-        dataset = data;
-    });
+  .then(response => response.json())
+  .then(data => {
+    dataset = data;
+  });
 
-document.getElementsById("symptomForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("symptomForm").addEventListener("submit", function(e) {
+  e.preventDefault();
 
-//retrieving symptoms selected by the user
-const selectedSymptoms = Array.from(
-    document.querySelectorAll("input[type=checkbox] : checked")
-).map(cb = cb.value);
+  // retrieving symptoms selected by the user
+  const selectedSymptoms = Array.from(
+    document.querySelectorAll("input[type=checkbox]:checked")
+  ).map(cb => cb.value);
 
-const results = [];
+  const results = [];
 
-//compare dataset
-for(const condition in dataset){
+  // compare dataset
+  for (const condition in dataset) {
+
     const conditionSymptoms = dataset[condition].symptom || dataset[condition].symptoms;
 
     let matchCount = 0;
 
     selectedSymptoms.forEach(symptom => {
-        if (conditionSymptoms.includes(symptom)){
-            matchCount++;
-        }
+      if (conditionSymptoms.includes(symptom)) {
+        matchCount++;
+      }
     });
 
-    const score = matchCount/conditionSymptoms.lenght;
+    const score = matchCount / conditionSymptoms.length;
 
     results.push({
-        condition : condition,
-        score : score,
-        description : dataset[condition].description
+      condition: condition,
+      score: score,
+      description: dataset[condition].description
     });
-}
-//arrange the scores from hightest to lowest
-    results.sort((a,b) => b.score - a.score);
-    displayResults(results.slice(0,3));
+  }
+
+  // arrange the scores from highest to lowest
+  results.sort((a,b) => b.score - a.score);
+
+  displayResults(results.slice(0,3));
 });
 
-//display function
+// display function
 
 function displayResults(results) {
 
